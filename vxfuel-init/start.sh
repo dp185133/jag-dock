a202b37d6d6f30636f9ec4d27d78601807dc4386
Pavel Kupcik <pk185092@ncr.com>
2024-04-10 09:47:32 -0400

    Added fluentbit sidecar and vxfuel-init container

---
+++ vxfuel-init/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#create directories needed by fluentbit container
+echo "Starting: Create fluentbit folders"
+mkdir -p /persist/fluentbit/db
+mkdir -p /persist/fluentbit/log
+echo "Finished: Create fluentbit folders"
