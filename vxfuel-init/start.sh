fd4d117b85da34d6681af852721215a29f187d84
MattPlacek <aphinmi@gmail.com>
2024-04-11 15:17:59 -0400

    change startup script to sh and switch to pcrfuel user

--- vxfuel-init/start.sh
+++ vxfuel-init/start.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
 #create directories needed by fluentbit container
 echo "Starting: Create fluentbit folders"
 mkdir -p /persist/fluentbit/db
 mkdir -p /persist/fluentbit/log
 echo "Finished: Create fluentbit folders"
+
+tail -F /dev/null
